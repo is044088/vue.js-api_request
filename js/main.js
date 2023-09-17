36d703469ad17b0925390905522a2e1f1d2d494f
KOKI　YUTO <52908553+is044088@users.noreply.github.com>
2023-09-17 21:06:07 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+  data: () => ({
+    items: null,
+    keyword: '',
+    message: ''
+  }),
+  watch: {
+    
+  },
+  moutend: function() {
+
+  },
+  methods: {
+
+  }
+})
+app.mount('#app')
